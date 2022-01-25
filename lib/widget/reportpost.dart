d168c78cdac3d6e01d4b850d0d314f9d6ab8c2a2
Sahil <sahilkumar1129@gmail.com>
2022-01-24 20:35:20 -0600

    This is good enough

--- lib/widget/reportpost.dart
+++ lib/widget/reportpost.dart
@@ -5,10 +5,22 @@ class ReportPost extends StatelessWidget {
   ReportPost(this.uuid);
   final String uuid;
 
-  launchUrl() async {
+  launchUrl(context) async {
     String emailUrl = 'mailto:pubcoasters@gmail.com?subject=Report Post: $uuid';
-    print(await canLaunch(emailUrl));
-    await launch(emailUrl);
+    if (await canLaunch(emailUrl)) {
+      await launch(emailUrl);
+    } else {
+      final snackBar = SnackBar(
+          content: Text(
+              'Error opening up the mail app. Feel free to email us at pubcoasters@gmail.com. Either describe the post or let us check it out by giving us this number: $uuid',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 20)),
+          backgroundColor: Colors.red);
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 
   @override
@@ -16,7 +28,7 @@ class ReportPost extends StatelessWidget {
     return IconButton(
       icon: Icon(Icons.warning),
       onPressed: () {
-        launchUrl();
+        launchUrl(context);
       },
       tooltip: 'Report Post',
       color: Colors.white,
